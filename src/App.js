58c8b3da45d4c2ee4386d4434143b39d1d7c98b9
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-10-19 00:23:57 +0600

    working on layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import { Container } from "react-bootstrap";
 import Main from "./pages/Main";
 
 function App() {
   return (
-    <div>
+    <Container className="mt-5">
       <Main />
-    </div>
+    </Container>
   );
 }
 
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
-import SidebarNav from '../components/header/SidebarNav';
+import Header from '../components/header/Header';
 
 const Main = () => {
     return (
         <section>
             <Row className="g-0">
                 <Col md={12} lg={3}>
-                    <Row>
-                        <Col xs={2}>
-                            <SidebarNav />
-                        </Col>
-                        <Col md={10}>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita tempore nisi nam sapiente nulla modi. Cumque voluptates praesentium temporibus labore atque tempore id maiores autem laudantium, ab placeat, assumenda sapiente molestiae ratione voluptatibus consectetur ipsum sed sint! Omnis assumenda error quaerat quam fugiat. Perferendis, modi! Iure impedit tempore ipsam adipisci dolores, fuga eius, assumenda pariatur, blanditiis aperiam odit? Dolor voluptatibus non molestias obcaecati unde dolorum expedita velit impedit sunt eius perferendis quibusdam incidunt ab sint ipsum, porro a odit nam officia, dolore aut illum commodi. Velit culpa, animi vitae consequuntur aliquam cumque libero voluptate quia ab amet voluptates eos odit.
-                        </Col>
-                    </Row>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita tempore nisi nam sapiente nulla modi. Cumque voluptates praesentium temporibus labore atque tempore id maiores autem laudantium, ab placeat, assumenda sapiente molestiae ratione voluptatibus consectetur ipsum sed sint! Omnis assumenda error quaerat quam fugiat. Perferendis, modi! Iure impedit tempore ipsam adipisci dolores, fuga eius, assumenda pariatur, blanditiis aperiam odit? Dolor voluptatibus non molestias obcaecati unde dolorum expedita velit impedit sunt eius perferendis quibusdam incidunt ab sint ipsum, porro a odit nam officia, dolore aut illum commodi. Velit culpa, animi vitae consequuntur aliquam cumque libero voluptate quia ab amet voluptates eos odit.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus dignissimos nobis fugit porro dolore placeat aperiam sed laborum et eaque ab doloribus iusto maiores expedita illum quos iste accusamus, perspiciatis nesciunt! Quas ut excepturi dignissimos expedita odio sint aut fugiat.
                 </Col>
-                <Col md={12} lg={9} className="bg-dark text-light">
+                <Col md={12} lg={9} className="bg-dark text-light d-none d-lg-block">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae hic consequuntur id excepturi libero sed ex dolor neque architecto. Nesciunt, eveniet fugiat unde odit sed perferendis laborum illum eaque inventore maxime expedita ipsum repudiandae cupiditate? Enim aperiam cupiditate ipsum reprehenderit eveniet ullam officia provident veritatis, omnis incidunt beatae possimus sint? Fugiat hic, adipisci consequatur animi voluptatum, incidunt quae harum nisi doloremque sed, consequuntur laboriosam vel saepe velit voluptatem quo eius facilis nulla rerum maxime tempore enim quod. Quae iste, harum quibusdam animi veniam molestias voluptate? Perspiciatis, provident voluptatum voluptas beatae reiciendis repudiandae fugit cumque animi officiis. Quos, neque aspernatur doloribus illo, quod voluptate dicta, magni totam laudantium qui illum. Ducimus, nihil similique fugiat, fuga atque impedit ex nesciunt earum dolorem natus tenetur doloribus necessitatibus qui repudiandae, consequatur consectetur est hic eligendi animi labore? Dicta rem dolore natus quis repudiandae eveniet molestiae, alias molestias qui doloremque sequi aperiam deleniti ab et blanditiis necessitatibus, eaque ipsam commodi illum harum hic error. Corrupti dolorem, quasi impedit praesentium perferendis deleniti fugiat porro voluptate error quas nobis nulla labore unde asperiores vel eligendi officiis exercitationem laudantium illum iste nihil voluptatibus sint incidunt rerum. Delectus, eum tempora. Error qui ex nam. Laborum magni aliquam quas eveniet?
                 </Col>
             </Row>
