3f5114f5037faf86d26db3bb5c385d2392feab1d
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-10-19 17:39:58 +0600

    bottom navigation almost complate

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -22,10 +22,10 @@ const Header = () => {
                 <div className="custom-profile">
                     <Link to="/">
                         <img
-                            src="https://i.ibb.co/cycQRDb/Chat-Cloud.png"
-                            width="53"
-                            height="53"
-                            className="d-inline-block align-top"
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6o04d9prZDVWlxJpl_atIHLQ6gUxxjjku4kpkRRozFXmx3LRqeuVLKiG2XIgq7U-nQTk&usqp=CA"
+                            width="50"
+                            height="50"
+                            className=" rounded-circle border border-2"
                             alt="ChatCloud_logo"
                         />
                     </Link>
