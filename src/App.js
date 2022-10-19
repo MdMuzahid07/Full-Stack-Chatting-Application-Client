16aa24855c4a5e609575d6744ac4cccc0754801b
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-10-19 23:08:51 +0600

    sass install and make index.css to index.scss

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import { Container } from "react-bootstrap";
 import BottomNav from "./components/header/BottomNav";
 import Main from "./pages/Main";
 
 
 function App() {
   return (
-    <Container className=" border border-2 px-0">
+    <section className=" border border-2 px-0">
       <Main />
       <BottomNav />
-    </Container>
+    </section>
   );
 }
 
