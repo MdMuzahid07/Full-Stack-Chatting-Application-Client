903c3c7f6d90319da3ce6c3adb6dd83cc4d6522d
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-10-19 12:41:37 +0600

    all routing added on sidebar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { Container } from "react-bootstrap";
 import Header from "./components/header/Header";
 import Main from "./pages/Main";
 
+
 function App() {
   return (
-    <Container className="mt-lg-5">
-      <Header />
+    <section>
       <Main />
-    </Container>
+      <Header />
+    </section>
   );
 }
 
