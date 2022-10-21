2d9b87ef2ba331986c6f597cfb18d9d8238f4dc2
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-10-21 17:27:14 +0600

    add friend page maked

--- src/pages/NavSidebar/AddFriends.js
+++ src/pages/NavSidebar/AddFriends.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import FriendProfile from '../../components/chatComponents/FriendProfile';
 
 const AddFriends = () => {
     return (
         <section>
             <div style={{ minHeight: "200px", width: "100%" }} className="sticky-top bg-lightBlue p-4 border-bottom border-light">
-                <p className="h4 mb-3">Add Friend</p>
+                <p className="h4 mb-3">Add Friends</p>
                 <div className="mt-3">
                     <div className="position-relative">
                         <input style={{ width: "100%", height: "40px" }} className="border-0 bg-light rounded-2 p-3 px-2" type="text" name="search" placeholder="search by name or id" />
@@ -12,12 +13,20 @@ const AddFriends = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="m19.6 21.225-6.35-6.35q-.75.575-1.725.925-.975.35-2.075.35-2.775 0-4.7-1.938Q2.825 12.275 2.825 9.5q0-2.775 1.925-4.713Q6.675 2.85 9.45 2.85q2.775 0 4.713 1.937Q16.1 6.725 16.1 9.5q0 1.1-.337 2.075-.338.975-.913 1.7l6.325 6.35ZM9.45 13.85q1.825 0 3.1-1.263 1.275-1.262 1.275-3.087 0-1.825-1.275-3.088-1.275-1.262-3.1-1.262-1.825 0-3.087 1.262Q5.1 7.675 5.1 9.5q0 1.825 1.263 3.087Q7.625 13.85 9.45 13.85Z" /></svg>
                         </span>
                     </div>
+                    <div className="d-flex justify-content-between mt-4">
+                        <p className="h5">Suggested</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="m15 21.2-6-2.1-4.575 1.8q-.6.225-1.113-.138Q2.8 20.4 2.8 19.75v-14q0-.4.225-.7.225-.3.6-.45L9 2.8l6 2.1 4.575-1.8q.6-.225 1.113.137.512.363.512 1.013v14q0 .4-.225.7-.225.3-.6.45Zm-1-2.725V6.925l-4-1.4v11.55Zm2 0 2.925-.975V5.8L16 6.925ZM5.075 18.2 8 17.075V5.525L5.075 6.5ZM16 6.925v11.55Zm-8-1.4v11.55Z" /></svg>
+                    </div>
                 </div>
             </div>
             <div className="p-4">
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa distinctio possimus autem veritatis tempora ea accusamus asperiores repudiandae quas sint, nisi quidem placeat sapiente eius totam aut quae doloremque ab recusandae nemo quam? Saepe explicabo harum ut eum numquam, rerum soluta delectus cumque quos fugiat assumenda ab! Magnam doloribus necessitatibus deleniti sed. Saepe exercitationem ut ex vero quisquam tempora odit perspiciatis numquam temporibus debitis dolores vitae iure laudantium voluptas possimus animi, molestiae voluptatum necessitatibus tempore dolor. Dolore natus libero blanditiis ab! Libero iusto sunt ea odit unde expedita consequatur, iure neque harum atque repellendus dolore, temporibus quisquam. Tempora recusandae doloribus ex dicta sapiente corrupti blanditiis natus molestias consectetur cum harum esse neque reprehenderit id et eius, similique magnam quaerat praesentium a fugit eligendi odio. Nam architecto tempora id. Voluptate mollitia minima necessitatibus vero fuga reprehenderit harum impedit aspernatur quis molestiae excepturi nam unde, itaque, voluptates, illum vitae voluptas. Molestiae iure quod dolorem possimus error nihil doloribus vel! Necessitatibus nam ratione quod fugiat, molestiae recusandae. Sequi temporibus iusto quis saepe, inventore fuga nemo deserunt atque animi. Harum eligendi repellat numquam quibusdam eius? Rerum modi rem similique voluptate optio? Quaerat sit ipsa sunt nihil fugiat similique dolorum commodi, nemo aperiam, maiores nostrum sint dolor libero, animi quas. Iusto fugiat et quod soluta, repellat porro nesciunt excepturi velit obcaecati sit laborum iure animi delectus earum nobis modi incidunt. Blanditiis, hic cumque? Maiores quam soluta quasi provident tempora suscipit eaque voluptatum sint rem velit hic, alias voluptatibus iste? Saepe, quam aperiam velit nemo vel placeat eos voluptatibus hic atque amet ratione, accusamus neque obcaecati earum ut dolor ad veritatis. Laborum debitis quibusdam laboriosam maxime voluptas vitae eum voluptate quas ad, fugit vero deleniti vel enim, ut quisquam dicta! Quasi iure vero nam nihil, non neque id praesentium dicta rerum in voluptates recusandae quis nisi cumque nemo? Eum placeat iusto, maiores fugiat aliquam tenetur harum, nemo, dolorem nesciunt fugit soluta? Nemo cumque molestias, voluptatibus a rem laudantium sapiente nobis magnam similique eos? Facilis molestiae, sunt praesentium sit explicabo illum beatae ipsa aspernatur vero, unde, labore atque eius autem fugit. Provident amet eius iure et dignissimos voluptate asperiores, ullam, fugit officiis error ducimus iste totam. Voluptates amet tempore magni numquam quo adipisci nam minima nostrum odio ex illum ipsa possimus illo, dicta hic maiores id dolorum voluptatum quis? Suscipit hic consequatur totam alias libero velit voluptatibus! Nihil quod asperiores dicta ad fugit facilis at! Facilis, harum. Maxime tenetur laudantium facere nostrum nemo fuga necessitatibus esse, facilis voluptatem, at aperiam asperiores, sapiente vel atque natus omnis. Cumque neque minus libero commodi exercitationem culpa odio porro at alias veritatis similique praesentium, quod quia ipsum delectus aperiam nesciunt quaerat temporibus tenetur! Sint maiores ducimus tempora modi magni, doloremque quam repellat quas ea dolorum dignissimos molestiae quidem rem temporibus iusto? Asperiores, animi culpa praesentium odio delectus, dolor eaque consequuntur tempore inventore, excepturi alias officia ad dolorum voluptatum quidem earum maxime in nulla! Cumque, architecto eaque veniam labore a fugit quidem, inventore ipsum nemo omnis similique id alias voluptatum magnam sequi!
-                </p>
+                <FriendProfile />
+                <FriendProfile />
+                <FriendProfile />
+                <FriendProfile />
+                <FriendProfile />
+                <FriendProfile />
+                <FriendProfile />
             </div>
         </section>
     );
